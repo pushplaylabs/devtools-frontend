61b196faf997e93abe17d7d13a17326b07c5bd57
Ferunelli <ferunelli222@gmail.com>
2024-08-23 16:25:21 +0300

    feat: remove panels

--- front_end/entrypoints/shell/shell.ts
+++ front_end/entrypoints/shell/shell.ts
@@ -4,14 +4,6 @@
 
 import '../../Images/Images.js';
 import '../../core/dom_extension/dom_extension.js';
-import '../../panels/sources/sources-meta.js';
-import '../../panels/profiler/profiler-meta.js';
-import '../../panels/console/console-meta.js';
-import '../../panels/coverage/coverage-meta.js';
-import '../../panels/changes/changes-meta.js';
-import '../../panels/linear_memory_inspector/linear_memory_inspector-meta.js';
-import '../../panels/settings/settings-meta.js';
-import '../../panels/protocol_monitor/protocol_monitor-meta.js';
 import '../../models/persistence/persistence-meta.js';
 import '../../models/logs/logs-meta.js';
 import '../main/main-meta.js';
